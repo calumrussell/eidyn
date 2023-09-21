4e3851fad5d15ae44ab5aeaa188c366fb6a01d89
calum <calum.mj.russell@gmail.com>
2023-09-21 23:52:05 +0100

    update dbt models, fix export for static site

--- next/next.config.js
+++ next/next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   compiler: {
-      styledComponents: true
-   },
+  output: 'export',
+  compiler: {
+    styledComponents: true
+  },
 }
 
 module.exports = nextConfig
